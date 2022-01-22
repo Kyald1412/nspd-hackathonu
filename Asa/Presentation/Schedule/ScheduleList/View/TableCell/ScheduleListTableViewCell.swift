820744b6dedebb43b80ea54a4f2c12f16cf0e4ac
oddesa <jehnsen.hirena@sci.ui.ac.id>
2022-01-22 16:05:53 +0700

    add schedule list table view cell

---
+++ Asa/Presentation/Schedule/ScheduleList/View/TableCell/ScheduleListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ScheduleListTableViewCell.swift
+//  Asa
+//
+//  Created by Jehnsen Hirena Kane on 22/01/22.
+//
+
+import UIKit
+
+class ScheduleListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
