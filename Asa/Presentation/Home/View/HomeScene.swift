29b3957bddf1fcaf19a0d8709279385d95a05b0a
oddesa <jehnsen.hirena@sci.ui.ac.id>
2022-01-27 22:20:43 +0700

    add new home presentation

---
+++ Asa/Presentation/Home/View/HomeScene.swift
@@ -0,0 +1,29 @@
+//
+//  HomeScene.swift
+//  Asa
+//
+//  Created by Jehnsen Hirena Kane on 27/01/22.
+//
+
+import UIKit
+
+class HomeScene: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
